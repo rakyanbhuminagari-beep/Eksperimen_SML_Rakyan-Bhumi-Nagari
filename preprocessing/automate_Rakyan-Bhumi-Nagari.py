4b43541823622a4b98d240d543008db87f53161e
rakyanbhuminagari-beep <rakyanbhuminagari@gmail.com>
2026-07-03 14:04:34 +0700

    Add files via upload

---
+++ preprocessing/automate_Rakyan-Bhumi-Nagari.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+
+df = pd.read_csv('WA_Fn-UseC_-Telco-Customer-Churn.csv')
+
+df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+
+if 'customerID' in df.columns:
+    df = df.drop('customerID', axis=1)
+df['TotalCharges'] = df['TotalCharges'].fillna(0)
+binary_cols = ['gender', 'Partner', 'Dependents', 'PhoneService', 'PaperlessBilling', 'Churn']
+multiclass_cols = ['MultipleLines', 'InternetService', 'OnlineSecurity', 'OnlineBackup', 
+                   'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies', 
+                   'Contract', 'PaymentMethod']
+
+le = LabelEncoder()
+for col in binary_cols:
+    df[col] = le.fit_transform(df[col])
+
+df = pd.get_dummies(df, columns=multiclass_cols, drop_first=True)
+
+num_cols = ['tenure', 'MonthlyCharges', 'TotalCharges']
+scaler = StandardScaler()
+df[num_cols] = scaler.fit_transform(df[num_cols])
+
+output_filename = 'telco_churn_preprocessed.csv'
+df.to_csv(output_filename, index=False)
+
+df.head()
